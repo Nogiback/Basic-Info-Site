b00b8bdafe795d271ea050000bc079a086fc6092
Nogiback <peter.toan.do@gmail.com>
2024-01-21 22:28:57 -0500

    Transitioning app to use Express

--- index.js
+++ index.js
@@ -1,39 +1,53 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
-const PORT = 8080 || process.env.PORT;
+// Without Express
+// const http = require("http");
+// const fs = require("fs");
+// const url = require("url");
+// const PORT = 8080 || process.env.PORT;
 
-const page404 = fs.readFileSync("./pages/404.html", "utf-8", (err, data) => {
-  if (err) throw err;
-  return data;
+// const page404 = fs.readFileSync("./pages/404.html", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   return data;
+// });
+
+// const server = http.createServer((req, res) => {
+//   const query = url.parse(req.url, true);
+//   let filename = "";
+
+//   if (query.pathname === "/") {
+//     filename = "./pages/index.html";
+//   } else {
+//     filename = "./pages" + query.pathname + ".html";
+//   }
+
+//   fs.readFile(filename, (err, data) => {
+//     if (err) {
+//       res.writeHead(404, { "Content-Type": "text/html" });
+//       res.write(page404);
+//       return res.end();
+//     } else {
+//       res.writeHead(200, { "Content-Type": "text/html" });
+//       res.write(data);
+//       return res.end();
+//     }
+//   });
+// });
+
+// server.listen(PORT, (err) => {
+//   if (err) {
+//     console.log("Something went wrong", err);
+//   }
+//   console.log("Server is listening on port " + PORT);
+// });
+
+// With Express
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 
-const server = http.createServer((req, res) => {
-  const query = url.parse(req.url, true);
-  let filename = "";
-
-  if (query.pathname === "/") {
-    filename = "./pages/index.html";
-  } else {
-    filename = "./pages" + query.pathname + ".html";
-  }
-
-  fs.readFile(filename, (err, data) => {
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.write(page404);
-      return res.end();
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    }
-  });
-});
-
-server.listen(PORT, (err) => {
-  if (err) {
-    console.log("Something went wrong", err);
-  }
-  console.log("Server is listening on port " + PORT);
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
